afc1b4f3a447d07492122850d071a9c38cb93cd1
Alex198919891989 <134425725+Alex198919891989@users.noreply.github.com>
2023-06-14 21:40:07 +0300

    +

--- 4.1.py
+++ 4.1.py
@@ -1,4 +1,26 @@
+import sqlite3
+connection = sqlite3.connect('teachers.db')
+cursor = connection.cursor()
+query = """CREATE TABLE Students (
+Student_Id INTEGER NOT NULL,
+Student_Name TEXT NOT NULL,
+School_Id INTEGER NOT NULL PRIMARY KEY);"""
+cursor.execute(query)
+connection.commit()
+connection.close()
 
+import sqlite3
+connection = sqlite3.connect('teachers.db')
+cursor = connection.cursor()
+query = """INSERT INTO Students (Student_Id , Student_Name , School_Id)
+VALUES
+(201, 'Иван', 1),
+(202, 'Петр', 2),
+(203, 'Анастасия', 3),
+(204, 'Игорь', 4);"""
+cursor.execute(query)
+connection.commit()
+connection.close()
 
 import sqlite3
 
